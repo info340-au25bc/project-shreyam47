20d0b9529302ac6a10f24d2b6ffb003f7b43170e
Jacob  Frans <jacobfrans@Jacobs-MacBook-Pro-51.local>
2025-12-04 21:27:47 -0800

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5yC8rNve-nytwxp_QHagTmZLa9VF_064",
+  authDomain: "greenthread-info340.firebaseapp.com",
+  projectId: "greenthread-info340",
+  storageBucket: "greenthread-info340.firebasestorage.app",
+  messagingSenderId: "1001326053172",
+  appId: "1:1001326053172:web:298a5e52332957b6fc0761"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
